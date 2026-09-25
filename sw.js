5baf4d3382ca966905c24f76625cfab5a43a65c7
luca1006-m <8m25pktgry@privaterelay.appleid.com>
2026-09-25 09:23:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,51 +1,48 @@
-const CACHE = 'autocheck-v4';
+const CACHE='autocheck-v9';
 
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icon.svg'
-];
-
-self.addEventListener('install', event => {
+self.addEventListener('install',event=>{
   self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate',event=>{
   event.waitUntil(
-    caches.keys().then(keys =>
+    caches.keys().then(keys=>
       Promise.all(
         keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
+          .filter(k=>k!==CACHE)
+          .map(k=>caches.delete(k))
       )
-    ).then(() => self.clients.claim())
+    ).then(()=>self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
 
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
+  const url=new URL(event.request.url);
 
-        caches.open(CACHE)
-          .then(cache => cache.put(event.request, copy))
-          .catch(() => {});
-
-        return response;
-      })
-      .catch(() =>
-        caches.match(event.request)
-          .then(response =>
-            response || caches.match('./index.html')
-          )
-      )
-  );
+  if(
+    url.pathname.endsWith('/') ||
+    url.pathname.endsWith('/index.html')
+  ){
+    event.respondWith(
+      fetch(event.request,{cache:'no-store'})
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+          return response;
+        })
+        .catch(()=>caches.match(event.request))
+    );
+  }else{
+    event.respondWith(
+      fetch(event.request)
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+          return response;
+        })
+        .catch(()=>caches.match(event.request))
+    );
+  }
 });
